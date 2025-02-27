ebbd078b9e2020c7e968f517e6360f54b5126f02
skmoin97 <skmoinuddin1997@gmail.com>
2025-02-27 12:23:42 +0530

    Add files via upload
    All files are uploaded successfully.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let mainListDiv = document.getElementById("navmenulist"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("fa-x");
+    
+};
